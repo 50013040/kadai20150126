694ad7c98f746de42ca0323315ac8cd219b260bb
50013040 <one_thirty_forty@yahoo.co.jp>
2015-01-28 14:35:10 +0900

    20150128 submit

---
+++ src/graph/Graph_main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+/**
+ *
+ * @author Henri_2
+ */
+public class Graph_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Graph_view graph = new Graph_view();
+        graph.setBounds(5,5,655,455);
+        graph.setVisible(true);
+    }
+    
+}
---
+++ src/graph/Graph_view.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+/**
+ *
+ * @author Henri_2
+ */
+public class Graph_view extends Frame implements ActionListener,WindowListener,KeyListener{
+
+    public Graph_view(){
+        addWindowListener(this);
+        setTitle("Graph");
+        
+        DefaultCategoryDataset data = new DefaultCategoryDataset();
+        
+        
+        data.addValue(300,"US","2005");
+        data.addValue(500,"US","2006");
+        data.addValue(120,"US","2007");
+        
+        data.addValue(200,"China","2005");
+        data.addValue(400,"China","2006");
+        data.addValue(320,"China","2007");
+        
+        JFreeChart chart = 
+        ChartFactory.createLineChart("import",
+                "year",
+                "ton(t)",
+               data,
+               PlotOrientation.VERTICAL,
+               true,
+               false,
+               false);
+
+   ChartPanel cpanel = new ChartPanel(chart);
+   add(cpanel, BorderLayout.CENTER);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
